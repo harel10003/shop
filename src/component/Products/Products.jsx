48e3dfa656985f582924323d5004bc11f28a4a4a
harel sela <harel10003@gmail.com>
2022-02-23 11:22:46 +0200

    new shop

---
+++ src/component/Products/Products.jsx
@@ -0,0 +1,20 @@
+import Product from '../Product/Product';
+import './Products.css';
+
+function Products() {
+	return (
+		<section className="products">
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+		</section>
+	);
+}
+
+export default Products;
---
+++ src/component/Todos/Todos.jsx
@@ -0,0 +1,15 @@
+import './Todos.css';
+
+function Todos() {
+	return (
+		<div>
+			Todos
+			<input
+				className="Todos-input"
+				type="text"
+				placeholder="enter somethings"
+			/>
+		</div>
+	);
+}
+export default Todos;
