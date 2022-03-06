e930d60eac2e25770f7e5adb0cb7597ba7094826
harel sela <harel10003@gmail.com>
2022-03-06 19:00:41 +0200

    gg

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,41 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import Modal from '../component/About/Modal';
+import { Global } from '@emotion/react';
+import { GlobalStyle } from '../component/globalStyled';
+
 function About() {
-	return <div>About</div>;
+	const [showModal, setShowModal] = useState(false);
+
+	const Container = styled.div`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	`;
+
+	const Button = styled.button`
+		min-width: 100px;
+		padding: 16px 32px;
+		border-radius: 4px;
+		background: black;
+		color: white;
+		font-size: 24px;
+		cursor: pointer;
+	`;
+
+	const openModal = () => {
+		setShowModal((prev) => !prev);
+	};
+	return (
+		<>
+			<Container>
+				<Button onClick={openModal}>i`m a modal</Button>
+				<Modal showModal={showModal} setShowModal={setShowModal} />
+				<GlobalStyle />
+			</Container>
+		</>
+	);
 }
 
 export default About;
