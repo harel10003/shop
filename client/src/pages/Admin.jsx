2a790d76f19d6674c2d995cca9e717c580df00b6
harel10003 <harel10003@gmail.com>
2022-04-25 12:39:26 +0300

    update

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,123 @@
+import { TextField } from '@mui/material';
+import { useContext } from 'react';
+import shopContext from '../component/context/ShopConetext';
+
 function Admin() {
-	return <>Admin</>;
+	const { productsList, setProductsList, setFilterdList } =
+		useContext(shopContext);
+
+	let newProduct = {
+		title: '',
+		description: '',
+		category: '',
+		image: '',
+		price: '',
+		rating: {
+			rate: '',
+			count: '',
+		},
+	};
+	const post = () => {
+		fetch('/api/products', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				title: newProduct.title,
+				description: newProduct.description,
+				category: newProduct.category,
+				image: newProduct.image,
+				price: newProduct.price,
+				rating: {
+					rate: newProduct.rating.rate,
+					count: newProduct.rating.count,
+				},
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => setProductsList([data, ...productsList]));
+	};
+	const put = (id) => {
+		// fetch('/api/products/:id', {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		Accept: 'application/json',
+		// 		'Content-Type': 'application/json',
+		// 	},
+		// 	body: JSON.stringify({
+		// 		title: newProduct.title,
+		// 		description: newProduct.description,
+		// 		category: newProduct.category,
+		// 		image: newProduct.image,
+		// 		price: newProduct.price,
+		// 		rating: {
+		// 			rate: newProduct.rating.rate,
+		// 			count: newProduct.rating.count,
+		// 		},
+		// 	}),
+		// })
+		// 	.then((res) => res.json())
+		// 	.then((data) => setProductsList([data, ...productsList]));
+	};
+	const deleted = (id) => {};
+
+	// const removeTodo = (id) => {
+	// 	setTodos(todos.filter((todo) => todo.id !== id));
+	//   };
+
+	//   const toggleTodo = (id) => {
+	// 	setTodos(
+	// 	  todos.map((todo) =>
+	// 		todo.id === id ? { ...todo, completed: !todo.completed } : todo
+	// 	  )
+	// 	);
+	//   };
+
+	// title, description, category, image, price, rating
+	return (
+		<>
+			<div>new product</div>
+			<TextField
+				onChange={(e) => (newProduct.title = e.target.value)}
+				labal="New title"
+				placeholder="New title"
+			/>
+			<TextField
+				onChange={(e) => (newProduct.description = e.target.value)}
+				labal="New description"
+				placeholder="New description"
+			/>
+			<TextField
+				onChange={(e) => (newProduct.category = e.target.value)}
+				labal="New category"
+				placeholder="New category"
+			/>
+			<TextField
+				onChange={(e) => (newProduct.image = e.target.value)}
+				labal="New image"
+				placeholder="New image"
+			/>
+			<TextField
+				onChange={(e) => (newProduct.price = e.target.value)}
+				labal="New price"
+				placeholder="New price"
+			/>
+			<TextField
+				onChange={(e) => (newProduct.rating.rate = e.target.value)}
+				labal="New rate"
+				placeholder="New rate"
+			/>
+			<TextField
+				onChange={(e) => (newProduct.rating.count = e.target.value)}
+				labal="New count"
+				placeholder="New count"
+			/>
+
+			<button onClick={() => post()}>add product</button>
+		</>
+	);
 }
 
 export default Admin;
